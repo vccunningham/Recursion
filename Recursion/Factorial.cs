7177054ada9460be12d7c19251896ef4622f6ceb
max-student <max-student@maxtrain.com>
2020-02-19 14:06:59 -0500

    Add project files.

---
+++ Recursion/Factorial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recursion {
+    public class Factorial {
+
+        public static int factorial(int n) {
+            if (n == 1) {
+                return 1;
+            }
+            return n * factorial(n - 1);
+        }
+
+    }
+}
---
+++ Recursion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Recursion {
+    class Program {
+        static void Main(string[] args) {
+
+            var fact = Factorial.factorial(10);
+            Console.WriteLine($"10! = {fact}");
+
+        }
+    }
+}
